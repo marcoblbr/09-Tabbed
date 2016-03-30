51d684c7550da486a3eccea80316a11c716dac6b
marco <marcolinhares@Marcos-MacBook-Pro.local>
2016-03-30 17:51:19 -0300

    commit
    commit

---
+++ 09 - Tabbed App/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  09 - Tabbed App
+//
+//  Created by Marco Linhares on 6/13/15.
+//  Copyright (c) 2015 Marco Linhares. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println ("First view loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 09 - Tabbed App/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  09 - Tabbed App
+//
+//  Created by Marco Linhares on 6/13/15.
+//  Copyright (c) 2015 Marco Linhares. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println ("Second view loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
